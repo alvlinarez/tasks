02ca4b0512e3f250955dd05e4afe71db4a6c0fbf
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-10-12 11:13:36 -0500

    Axios url updated

--- config/axios.js
+++ config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosClient = () => {
   return axios.create({
     //baseURL: 'http://localhost:5000/api/',
-    baseURL: 'https://alg-tasks-server.herokuapp.com/',
+    baseURL: 'https://alg-tasks-server.herokuapp.com/api/',
     withCredentials: true
   });
 };
